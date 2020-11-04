d86b3fcd9becf555f741901d407482fb6bd916e6
rohit <rohitchatla@gmail.com>
2020-11-04 15:02:41 +0530

    online status added,etc

--- config/config.js
+++ config/config.js
@@ -1,16 +1,8 @@
 const config = {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3001,
-  jwtSecret: process.env.JWT_SECRET || "dfjnsdjkfndjkfndj",
-  mongoUri:
-    process.env.MONGODB_URI ||
-    "mongodb+srv://rohit:rohit@cluster0.hfpne.mongodb.net/cvrrsocial?retryWrites=true&w=majority",
-  // process.env.MONGO_HOST ||
-  // "mongodb://" +
-  //   (process.env.IP || "localhost") +
-  //   ":" +
-  //   (process.env.MONGO_PORT || "27017") +
-  //   "/cvrrsocial",
+  jwtSecret: process.env.JWT_SECRET,
+  mongoUri: process.env.MONGODB_URI,
 };
 
 export default config;
