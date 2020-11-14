cd80f7798ac297e555ca5b928765d4405db92354
rohit <rohitchatla@gmail.com>
2020-11-14 13:24:32 +0530

    new

--- config/config.js
+++ config/config.js
@@ -1,8 +1,10 @@
 const config = {
   env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 3000,
-  jwtSecret: process.env.JWT_SECRET,
-  mongoUri: process.env.MONGODB_URI,
+  jwtSecret: process.env.JWT_SECRET || "emsifmdmfasdkmfalkdsmfkls",
+  mongoUri:
+    process.env.MONGODB_URI ||
+    "mongodb+srv://rohit:rohit@cluster0.hfpne.mongodb.net/cvrrsocial?retryWrites=true&w=majority",
 };
 
 export default config;
